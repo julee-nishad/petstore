79072259daf6103f2b93e9eda9d88e0159a13b15
Rupali <rupali.nishad121@gmail.com>
2023-08-17 20:59:53 +0530

    Initial commit

---
+++ src/main/java/com/itvedant/petstore79/Petstore79Application.java
@@ -0,0 +1,13 @@
+package com.itvedant.petstore79;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Petstore79Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Petstore79Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/itvedant/petstore79/Petstore79ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itvedant.petstore79;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Petstore79ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
